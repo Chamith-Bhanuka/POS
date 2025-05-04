100c2e33938f5e2237ce31e8afe79741244a2f03
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-05-04 13:43:30 +0530

    create method to load customers to table

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -1,2 +1,22 @@
-import controller from '../db/db.js';
+import {customer_db} from '../db/db.js';
 import CustomerModel from "../model/CustomerModel.js";
+
+function loadCustomers() {
+    $('customer-tbody').empty();
+
+    customer_db.map((item, index) => {
+        let name  = item.custFullName;
+        let email = item.custEmail;
+        let phone = item.custPhone;
+        let address = item.custAddress;
+
+        let data = `<tr>
+                                <td>${index+1}</td>
+                                <td>${name}</td>
+                                <td>${email}</td>
+                                <td>${phone}</td>
+                                <td>${address}</td>
+                          </tr>`
+        $('customer-tbody').append(data);
+    })
+}
