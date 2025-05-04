bff4d497eaa79798aa57e973b0f4721489cff1ac
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-05-04 13:32:08 +0530

    create CustomerModel constructor

---
+++ model/CustomerModel.js
@@ -0,0 +1,8 @@
+export default class CustomerModel {
+    constructor(custFullName, custEmail, custPhone, custAddress) {
+        this.custFullName = custFullName;
+        this.custEmail = custEmail;
+        this.custPhone = custPhone;
+        this.custAddress = custAddress;
+    }
+}
