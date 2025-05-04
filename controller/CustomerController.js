b516bb277088ad224ab7aeefc24c4d025a78506b
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-05-04 14:05:47 +0530

    create customer save function

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -2,7 +2,7 @@ import {customer_db} from '../db/db.js';
 import CustomerModel from "../model/CustomerModel.js";
 
 function loadCustomers() {
-    $('customer-tbody').empty();
+    $('#customer-tbody').empty();
 
     customer_db.map((item, index) => {
         let name  = item.custFullName;
@@ -17,6 +17,42 @@ function loadCustomers() {
                                 <td>${phone}</td>
                                 <td>${address}</td>
                           </tr>`
-        $('customer-tbody').append(data);
+        $('#customer-tbody').append(data);
     })
 }
+
+//save customer
+
+$('#customer-save').on('click', function(){
+
+    console.log('customer save clicked');
+
+    let name = $('#custFullName').val();
+    let email = $('#custEmail').val();
+    let phone = $('#custPhone').val();
+    let address = $('#custAddress').val();
+
+    if (name === '' || email === '' || phone === '' || address === '') {
+        Swal.fire({
+            title: 'Error!',
+            text: 'Invalid Inputs',
+            icon: 'error',
+            confirmButtonText: 'Ok'
+        })
+    } else {
+        let customer_data = new CustomerModel(name, email, phone, address);
+
+        customer_db.push(customer_data);
+
+        console.log(customer_db);
+
+        loadCustomers();
+
+        Swal.fire({
+            title: "Added Successfully!",
+            icon: "success",
+            draggable: true
+        });
+    }
+
+});
