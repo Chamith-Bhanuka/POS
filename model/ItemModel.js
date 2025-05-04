48e62343c27f96678079f9d34c54fcc2c5dcc4da
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-05-04 23:34:11 +0530

    create itemModel

---
+++ model/ItemModel.js
@@ -0,0 +1,9 @@
+export default class ItemModel {
+    constructor(itemName, itemId, price, category, description) {
+        this.itemName = itemName;
+        this.itemId = itemId;
+        this.price = price;
+        this.category = category;
+        this.description = description;
+    }
+}
